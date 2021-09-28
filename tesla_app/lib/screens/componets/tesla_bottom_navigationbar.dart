21482d99945635ad578e56b6d2ec5848467db055
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-28 13:05:39 +0200

    Edited class tesla_bottom_navigatorbar

--- tesla_app/lib/screens/componets/tesla_bottom_navigationbar.dart
+++ tesla_app/lib/screens/componets/tesla_bottom_navigationbar.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constanins.dart';
+
+class TeslaBottomNavigationBar extends StatelessWidget {
+  const TeslaBottomNavigationBar({
+    Key? key,
+    required this.selectedTab,
+    required this.onTap,
+  }) : super(key: key);
+
+  final int selectedTab;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: onTap,
+      currentIndex: selectedTab,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.black,
+      items: List.generate(
+        navIconSrc.length,
+        (index) => BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            navIconSrc[index],
+            color: index == selectedTab ? primaryColor : Colors.white54,
+          ),
+          label: "",
+        ),
+      ),
+    );
+  }
+}
+
+List<String> navIconSrc = [
+  "assets/icons/Lock.svg",
+  "assets/icons/Charge.svg",
+  "assets/icons/Temp.svg",
+  "assets/icons/Tyre.svg",
+];
+
+/* 
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../contanins.dart';
 
 class TeslaBottomNavigationBar extends StatelessWidget {
@@ -17,7 +61,9 @@ class TeslaBottomNavigationBar extends StatelessWidget {
       currentIndex: selectedTab,
       type: BottomNavigationBarType.fixed,
       backgroundColor: Colors.black,
-      items: List.generate(navIconSrc.length, (index) => BottomNavigationBarItem(
+      items: List.generate(
+        navIconSrc.length,
+         (index) => BottomNavigationBarItem(
       icon: SvgPicture.asset(
         navIconSrc[index],
         color: index == selectedTab ? primaryColor : Colors.white54,
@@ -33,3 +79,4 @@ List <String> navIconSrc = [
   "assets/icons/Temp.svg"
   "assets/icons/Tyre.svg"
 ];
+*/
