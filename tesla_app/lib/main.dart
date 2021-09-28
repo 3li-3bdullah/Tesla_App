2eb9e4d60911b9b194b8f613515e57f09c1cfb2c
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-28 08:44:36 +0200

    Added black theme

--- tesla_app/lib/main.dart
+++ tesla_app/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
         primaryColor: Colors.blue,
       ),
       home: const HomeScreen(),
