8de0d8dbfccf987616a0325386498e948b05ec38
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-28 09:00:19 +0200

    Added stream and some codes

--- tesla_app/lib/screens/home_screen.dart
+++ tesla_app/lib/screens/home_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SafeArea(
+      child: LayoutBuilder(builder: (context, constrains) {
+          return Stack(
+            children: [
+             Padding(padding: EdgeInsets.symmetric(vertical: constrains.maxHeight * 0.1 ),
+             child:SvgPicture.asset(
+                "assets/icon/Car.svg",
+                width: double.infinity,
+              ),
+             )
+            ],
+            
+          );
+        }
+      ),
+    ))
+    ;
   }
 }
