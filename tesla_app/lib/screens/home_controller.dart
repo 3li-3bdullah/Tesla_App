f6cf4ecc8a051413dea379dd0ee6f1a73c948ae8
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-28 09:19:06 +0200

    Added new folder

---
+++ tesla_app/lib/screens/home_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeController extends ChangeNotifier {
+  // We use home controller for logical part
+  bool isRightDoorLock = true;
+
+  void updateRightDoorLock() {
+    isRightDoorLock = !isRightDoorLock;
+    // if you use provider then you may know this
+    // it work like setState
+  }
+}
