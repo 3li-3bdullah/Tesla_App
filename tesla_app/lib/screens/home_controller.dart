b643efd51bd656ffe61351d3d0062a7e8b5bac2a
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-28 10:22:27 +0200

    Added some feaures

--- tesla_app/lib/screens/home_controller.dart
+++ tesla_app/lib/screens/home_controller.dart
@@ -4,10 +4,17 @@ import 'package:flutter/material.dart';
 class HomeController extends ChangeNotifier {
   // We use home controller for logical part
   bool isRightDoorLock = true;
+  bool isLeftDoorLock = true;
 
   void updateRightDoorLock() {
     isRightDoorLock = !isRightDoorLock;
+    notifyListeners();
     // if you use provider then you may know this
     // it works like setState
+  }
+
+  void updateLeftDoorLock() {
+    isLeftDoorLock = !isLeftDoorLock;
+    notifyListeners();
   }
 }
