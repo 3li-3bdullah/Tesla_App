2d2d70279ec1fc4088456c5ffa9ed19dc1528173
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-28 08:37:48 +0200

    Added new fold and create file

---
+++ tesla_app/lib/contanins.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff53f9ff);
+const Color redColor = Color(0xffff5368);
+
+const double defaultPadding = 16.0;
+const Duration defaultDuration = Duration(milliseconds: 300);
